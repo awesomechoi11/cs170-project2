d3e44a67d46e77ff0c87e9ef3f9a4f956c46b0ca
brandon <brandonchoi2016@gmail.com>
2024-05-20 20:20:44 -0700

    node structure

--- FeatureSearch.py
+++ FeatureSearch.py
@@ -6,3 +6,14 @@
 # expand best child
 # repeat till last set
 
+
+
+class Node(object):
+    def __init__(self, feature):
+        self.parents = []
+        self.children = []
+        self.feature = feature
+    def add_child(self, obj):
+        self.children.append(obj)
+    def add_parent(self, obj):
+        self.parents.append(obj)
