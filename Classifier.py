f5fc7896feebe65624dcef54b76562aa8b3ba38f
ogod1 <ojasvigodha@gmail.com>
2024-06-02 18:49:53 -0700

    Added Train and Test functions to Classifier class

--- Classifier.py
+++ Classifier.py
@@ -1,5 +1,21 @@
 import numpy as np 
 
-#class Classifier:
+class Classifier:
+    def __init__(self):
+        self.trainingData = None
+        self.trainingLabels = None
 
+    def Train(self, trainingData, trainingLabels):
+        #storing the data and labels 
+        self.trainingData = np.array(trainingData)
+        self.trainingLabels = np.array(trainingLabels)
+
+    def Test(self, testInstance):
+        #classifying a test instance based on the NN from the training data
+        testInstance = np.array(testInstance)
+        distances = np.linalg.norm(self.trainingData - testInstance, axis=1)
+        nnIndex = np.argmin(distances)
+        return self.trainingLabels[nnIndex]
+    
+    
 #class Validator:
